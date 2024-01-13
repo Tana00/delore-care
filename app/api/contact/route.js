2abb2c6399962055e99ad2b7e056c7eeee8df2d1
happiness-balogun <happiness.balogun@optisourcetech.com>
2024-01-13 17:00:41 +0100

    update sendgrid api to and from emails

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -7,8 +7,8 @@ export async function POST(request) {
   const { body, subject, email, data } = await request.json();
 
   const res = await sendgrid.send({
-    to: data.email,
-    from: "happinessblgn0@gmail.com",
+    to: "info@delorecare.com",
+    from: data.email,
     subject: `${subject}`,
     html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html lang="en">
--- app/api/job-application/route.js
+++ app/api/job-application/route.js
@@ -16,8 +16,8 @@ export async function POST(request) {
   });
 
   const res = await sendgrid.send({
-    to: email,
-    from: "happinessblgn0@gmail.com",
+    to: "info@delorecare.com",
+    from: email,
     subject: `${subject}`,
     html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html lang="en">
